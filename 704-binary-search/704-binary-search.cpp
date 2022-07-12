169f7b17327a141beecbc6ef8c110ad674ef795f
Soohyeon B <56013677+SoohyeonB@users.noreply.github.com>
2022-07-12 22:45:46 +0900

    Time: 87 ms (5.83%), Space: 27.5 MB (63.77%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -1,25 +1,16 @@
-class Solution{
-public:
-	int search(vector<int>& nums, int target)
-	{
-		int left = 0;
-		int right = nums.size() - 1;
-
-		while (left <= right)
-		{
-			int pivot = (left + right) / 2;
-			if (nums[pivot] == target)
-			{
-				return pivot;
-			}
-			else if (nums[pivot] < target)
-			{
-				left = pivot + 1;
-			}
-			else
-			{
-				right = pivot - 1;
-			}
-		}return -1;
-	}
+class Solution {
+public: 
+    int search(vector<int>& nums, int target) {
+        int low = 0, high = nums.size()-1;
+        cout<< high;
+        while(low<=high){
+            int middle = (low+high)/2;
+            if(target == nums[middle]) return middle;
+            else if (target > nums[middle])
+                low = middle+1;
+            else high = middle-1;
+            
+        }
+        return -1;
+    }
 };

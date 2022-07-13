4cd4b3c7f5b0c2e44384a4286fd4ce961e652190
Soohyeon B <56013677+SoohyeonB@users.noreply.github.com>
2022-07-13 12:46:43 +0900

    Time: 0 ms (100.00%), Space: 6 MB (22.06%) - LeetHub

---
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -0,0 +1,23 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+//since the given array is sorted array, I will use binary search
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int left=1; 
+        int right = n;
+        int middle;
+        
+        while(left <right){
+            middle = left +(right-left)/2;
+            if(isBadVersion(middle))
+                right = middle;
+            else
+                left = middle+1;
+            
+        }
+        return right;
+    }
+};

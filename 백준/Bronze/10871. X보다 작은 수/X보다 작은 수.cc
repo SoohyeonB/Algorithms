347ef7fee29a074f562618bb468e4a4b20ab5cfe
Soohyeon B <56013677+SoohyeonB@users.noreply.github.com>
2022-10-13 07:01:10 +0900

    [Bronze V] Title: X보다 작은 수, Time: 420 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -0,0 +1,22 @@
+//2493 탑
+#include <iostream>
+#include <utility>
+#include <stack>
+
+using namespace std;
+
+int main(void) {
+  int n, x;
+  cin >> n >> x;
+  
+  
+  while(n--){
+    int t;
+    cin >> t;
+    if(t<x) cout<<t<<" ";
+    
+  }
+
+}
+
+

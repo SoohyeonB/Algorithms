0f29fbdb3b037c9888ceb32cc0b261cadd285043
Soohyeon B <56013677+SoohyeonB@users.noreply.github.com>
2022-10-17 21:01:40 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main (void){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n;
+    cin >> n;
+    int blank = (2*n-1)/2;
+    
+    //n줄 2n-1행 , 5줄, 9행 9/2 =4
+    for(int i=1; i<=n; i++, blank--){ //1 3 5 9 2n-1
+        for(int j=0; j<blank; j++) cout << " ";
+        for(int j=0; j<2*i-1; j++) cout << "*";
+        cout << "\n";
+    }
+    
+}

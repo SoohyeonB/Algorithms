be4e83c948ef3b3418edbb996bb4ac8bd2b8890f
Soohyeon B <56013677+SoohyeonB@users.noreply.github.com>
2022-11-08 07:13:48 +0900

    [Silver IV] Title: 큐 2, Time: 384 ms, Memory: 10424 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main (void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+  
+    int n;
+    cin >> n;
+    
+    queue<int> Q;
+    
+    while(n--){
+        string cmd;
+        cin >> cmd;
+        if(cmd =="push"){
+            int x;
+            cin >>x;
+            Q.push(x);
+        }
+        
+        else if(cmd =="pop"){
+            if(Q.empty()) cout <<-1<<'\n';
+            else{
+                cout << Q.front()<<'\n';
+                Q.pop();
+            }
+        }
+        else if(cmd =="size"){
+            cout << Q.size() <<'\n';
+        }
+        else if(cmd == "empty"){
+            if(Q.empty()) cout << 1<<'\n';
+            else cout << 0 <<'\n';
+        }
+        else if(cmd =="front"){
+            if(Q.empty()) cout << -1 <<'\n';
+            else cout << Q.front()<<'\n';
+        }
+        else{
+            if(Q.empty()) cout << -1<<'\n';
+            else cout << Q.back()<<'\n';
+        }
+    }
+    
+    return 0;
+}

f6cb31483fbc94861e91f222822434cfd4a712ad
Soohyeon B <56013677+SoohyeonB@users.noreply.github.com>
2024-02-05 23:59:00 +0900

    Time: 92 ms (94.01%), Space: 18.4 MB (77.24%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,24 +1,19 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        stack = []
-        volume = 0
+        if not height:
+            return 0
 
-        for i in range(len(height)):
-            # when meet inflection point
-            while stack and height[i] > height[stack[-1]]:
-                # pop stack
-                top = stack.pop()
-                print("top:", top)
-                # if stack is empty == 빗물을 가두지 못함
-                if not stack:
-                    break
-                
-                # 물 높이 업데이트
-                distance = i - stack[-1] -1
-                
-                print("height[top]", height[top])
-                waters = min(height[i], height[stack[-1]]) - height[top]
-
-                volume += distance * waters
-            stack.append(i)
-        return volume
+        left_index, right_index = 0, len(height) - 1
+        left_max, right_max = height[left_index], height[right_index]
+        blocks = 0
+        while left_index < right_index:
+            if left_max < right_max:
+                left_index += 1
+                # 현재 값과 이전 값 중 더 큰 것을 left_max로 업데이트함
+                left_max = max(left_max, height[left_index])
+                blocks += left_max - height[left_index]
+            else:
+                right_index -= 1
+                right_max = max(right_max, height[right_index])
+                blocks += right_max - height[right_index]
+        return blocks

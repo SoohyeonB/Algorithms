b8ea0f7b71f49a1c10a2ab11abf586501db03211
Soohyeon B <56013677+SoohyeonB@users.noreply.github.com>
2024-03-10 15:20:41 +0900

    [Gold V] Title: 강의실 배정, Time: 452 ms, Memory: 159336 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.py
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -1,21 +1,27 @@
 import sys
-from heapq import heappush, heappop
+import heapq
+from heapq import heappop, heappush
+
 input = sys.stdin.readline
 
 def solution():
     n = int(input())
-    classes = []
+    times = []
+    answer = 0
     for _ in range(n):
-        classes.append(list(map(int, input().split())))
-    classes.sort(key=lambda x: (x[0], x[1]))
+        times.append(list(map(int, input().split())))
     
-    end_time = [classes[0][1]]
+    # time을 min heap 형태로 정렬
+    times.sort(key=lambda x : (x[0], x[1]))
+    
+    # heap에 저장    
+    heap = [times[0][1]]
     for i in range(1, n):
-        if end_time[0] <= classes[i][0]:
-            heappop(end_time)
-        heappush(end_time, classes[i][1])
-        
+        if heap[0] <= times[i][0]:
+            heappop(heap)
+        heappush(heap, times[i][1])
+    answer = len(heap)
+    return answer
     
-    return len(end_time)
     
 print(solution())

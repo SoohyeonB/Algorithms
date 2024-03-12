b71e3da4cd1c3bb36e5cc9be22b67b3c55b23ffe
Soohyeon B <56013677+SoohyeonB@users.noreply.github.com>
2024-03-12 12:32:59 +0900

    Time: 480 ms (38.05%), Space: 17 MB (83.69%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -0,0 +1,18 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        count = collections.Counter(tasks)
+        # Initialize a max heap with negative counts
+        maxHeap = [-cnt for cnt in count.values()]
+        heapq.heapify(maxHeap)
+
+        time = 0 # time counter
+        q = deque() 
+        while maxHeap or q:
+            time += 1
+            if maxHeap:
+                cnt = 1 + heapq.heappop(maxHeap)
+                if cnt:
+                    q.append([cnt, time+n])
+            if q and q[0][1] == time:
+                heapq.heappush(maxHeap, q.popleft()[0])
+        return time

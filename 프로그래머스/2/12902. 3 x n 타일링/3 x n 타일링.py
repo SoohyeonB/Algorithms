ad52f0a36f17aae61c1c41e4f18130a9dd89c7bd
Soohyeon B <56013677+SoohyeonB@users.noreply.github.com>
2024-03-14 09:29:59 +0900

    [level 2] Title: 3 x n 타일링, Time: 282.31 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.py
+++ 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.py
@@ -1,13 +1,21 @@
 def solution(n):
-    dp = [0 for _ in range(n+1)]
-    if n%2 == 1:
-        return 0
-    dp[2] = 3
+    # 1. 점화식 : f(n) = f(n-2)*3 + 2*f(n-4) + 2f(n-6) + ... + 2f(2) + 2
+    # 2. 해당 식의 값을 각각 배열의 n번째에 저장
+    f = [0 for _ in range(n+1)]
+    
+    print(f)
+    
+    
+    # 1. n이 홀수이면 f(n) = 0
+    if n%2 == 1: return 0
+    f[2] = 3
     if n > 2:
-        for i in range(4, n+1, 2):
-            dp[i] = dp[i-2]*3 + 2
+        for i in range(4, n+2, 2):
+            f[i] = f[i-2]*3 +2
+            # print(f"{f[i]} = {f[i-2]}*3 + 2")
+            # i-4부터 0까지의 값을 2배 시키고 더하기
             for j in range(i-4, -1, -2):
-                dp[i] += dp[j] * 2
-                dp[i] %= 1000000007
-        
-    return dp[n]
+                f[i] += f[j] * 2
+            f[i] %=  1000000007
+    
+    return f[n]

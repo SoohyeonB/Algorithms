85e35d811608c6fdeb13249f8ab3315a3761c381
Soohyeon B <56013677+SoohyeonB@users.noreply.github.com>
2024-03-19 14:27:22 +0900

    Time: 26 ms (96.81%), Space: 16.5 MB (42.89%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,26 +1,31 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        def dfs(start, word):
-            # basecase
-            if len(digits) == len(word):
-                output.append(word)
+        # digits = "23"
+        # digits[i번째의 [n번째]]
+        # 1. 숫자와 알파벳 매칭
+        # 2. dfs로 순회
+            # 1) basecase: 중간 과정 string의 길이 == digits의 길이
+            # 2) for문으로 n 순회
+        result = []
+        dicts = {
+            "2":"abc",
+            "3":"def",
+            "4":"ghi",
+            "5":"jkl",
+            "6":"mno",
+            "7":"pqrs",
+            "8":"tuv",
+            "9":"wxyz"
+        }
+        # i: 0 ~ len(digits)
+        def dfs(i, subStr):
+            if len(subStr) == len(digits):
+                result.append(subStr)
                 return
-            for i in range(start, len(digits)):
-                for j in graph[digits[i]]:
-                    # 다음 단어를 찾으면서, 이번에 찾은 단어를 붙여서 보냄
-                    dfs(i+1, word+j)
-
-        graph = {'2': "abc", '3':"def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz"}
+            for c in dicts[digits[i]]:
+                # for c in "abc":
+                dfs(i+1, subStr + c)
         
-        # 결과가 담길 리스트
-        output = []
-
-        if not digits:
-            return output
-        # 중간에 생성된 단어를 저장할 공간
-        dfs(0, "")
-
-
-        return output
-        
-
+        if digits: 
+            dfs(0, "")
+        return result
